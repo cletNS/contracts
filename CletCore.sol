60449543ec6826858533e37ad70c7d8e1460577f
Franz Quarshie <franzquarshie@gmail.com>
2023-01-05 16:48:32 +0000

    auth-updates

--- CletCore.sol
+++ CletCore.sol
@@ -23,14 +23,18 @@ contract CLETCORE is Ownable {
 
     CletName[] public s_CletNames;
     Ticker[] public s_Tickers;
-    uint256 private constant TenPow18 = 10**18;
+    uint256 private constant TenPow18 = 10 ** 18;
     address private emptyAddress;
     address private constant VALIDATOR =
         0xf0802222a2908DF9B30b34076c95E39F375F46D9;
     mapping(string => bool) private name_Exists;
     mapping(string => MappedAddress) private name_ext_ToMap;
     mapping(string => address) private name_ToOwner;
-    mapping(address => string[]) private address_OwnedNames;
+    mapping(string => string) private name_Secret;
+    mapping(address => string) private address_Default;
+    mapping(address => mapping(string => string)) public address_key_name;
+    mapping(address => string[]) public address_OwnedNames;
+    mapping(address => string[]) private address_Keys;
     mapping(string => uint256) private name_ToID;
     mapping(string => string) private mapTo_name_ex;
 
@@ -64,11 +68,9 @@ contract CLETCORE is Ownable {
 
     /// @notice Returns the mapped details of a name.ticker
     /// @dev Returns type of MappedAddress
-    function resolve(string memory _name_ticker)
-        public
-        view
-        returns (MappedAddress memory)
-    {
+    function resolve(
+        string memory _name_ticker
+    ) public view returns (MappedAddress memory) {
         return name_ext_ToMap[_name_ticker];
     }
 
@@ -97,10 +99,10 @@ contract CLETCORE is Ownable {
     }
 
     /// @notice Transfers an existing owned name to another account
-    function transferName(string memory _name, address _newOwner)
-        public
-        isNameOwner(name_ToOwner[_name.toLower()])
-    {
+    function transferName(
+        string memory _name,
+        address _newOwner
+    ) public isNameOwner(name_ToOwner[_name.toLower()]) {
         CletName storage cletname = s_CletNames[name_ToID[_name]];
         deleteMapped(_name, cletname.owner);
         cletname.owner = _newOwner;
@@ -131,11 +133,9 @@ contract CLETCORE is Ownable {
     }
 
     /// @notice Returns names belonging to an account
-    function getOwnedNames(address _owner)
-        public
-        view
-        returns (string[] memory)
-    {
+    function getOwnedNames(
+        address _owner
+    ) public view returns (string[] memory) {
         uint256 resCount = 0;
         for (
             uint256 index = 0;
@@ -147,24 +147,28 @@ contract CLETCORE is Ownable {
             }
         }
         string[] memory data = new string[](resCount);
+        uint256 dataIndex = 0;
         for (
             uint256 index = 0;
             index < address_OwnedNames[_owner].length;
             index++
         ) {
             if (!address_OwnedNames[_owner][index].isEqual("")) {
-                data[index] = address_OwnedNames[_owner][index];
+                data[dataIndex] = address_OwnedNames[_owner][index];
+                dataIndex++;
             }
         }
         return data;
     }
 
+    function getOwnedNamesLength(address _owner) public view returns (uint256) {
+        return address_OwnedNames[_owner].length;
+    }
+
     /// @notice Returns all mappedInfo of a name
-    function getAllMappedInfo(string memory _name)
-        public
-        view
-        returns (string[] memory)
-    {
+    function getAllMappedInfo(
+        string memory _name
+    ) public view returns (string[] memory) {
         string[] memory data = new string[](s_Tickers.length);
         for (uint256 index = 0; index < s_Tickers.length; index++) {
             string memory name_ex = string.concat(
@@ -178,30 +182,72 @@ contract CLETCORE is Ownable {
     }
 
     /// @notice Returns the name belonging to a mapped information
-    function reverseLookup(string memory _address)
-        public
-        view
-        notEmpty(_address)
-        returns (string memory)
-    {
+    function reverseLookup(
+        string memory _address
+    ) public view notEmpty(_address) returns (string memory) {
         return mapTo_name_ex[_address];
+    }
+
+    /// @notice Sets the default name of an account
+    function setDefault(
+        string memory _name
+    ) public isNameOwner(name_ToOwner[_name.toLower()]) {
+        address_Default[msg.sender] = _name;
+    }
+
+    /// @notice Adds a key to a name
+    function setKey(
+        string memory _name,
+        string memory _key
+    ) public isNameOwner(name_ToOwner[_name.toLower()]) {
+        address_key_name[msg.sender][_key] = _name;
+        address_Keys[msg.sender].push(_key);
+    }
+
+    /// @notice Removes a key from a name
+    function removeKey(
+        string memory _name,
+        string memory _key
+    ) public isNameOwner(name_ToOwner[_name.toLower()]) {
+        address_key_name[msg.sender][_key] = "0";
+    }
+
+    /// @notice Adds a secret to a name
+    function setSecret(
+        string memory _hash,
+        string memory _name
+    ) public isNameOwner(name_ToOwner[_name.toLower()]) {
+        name_Secret[_name] = _hash;
+    }
+
+    /// @notice Validates a secret
+    function secretIsValid(
+        string memory _name,
+        string memory _secret
+    ) public view isNameOwner(name_ToOwner[_name.toLower()]) returns (bool) {
+        bool valid = false;
+        string memory _sHash = keccak256((abi.encodePacked(_secret))).toHex();
+        if (name_Secret[_name].isEqual(_sHash)) {
+            valid = true;
+        }
+        return valid;
     }
 
     function release(address _address, string memory _name) public onlyOwner {
         doRelease(_address, _name);
     }
 
-    function validatorRelease(address _address, string memory _name)
-        public
-        isValidator
-    {
+    function validatorRelease(
+        address _address,
+        string memory _name
+    ) public isValidator {
         doRelease(_address, _name);
     }
 
-    function doRelease(address _address, string memory _name)
-        private
-        noNull(_name)
-    {
+    function doRelease(
+        address _address,
+        string memory _name
+    ) private noNull(_name) {
         if (name_Exists[_name] == true) {
             releaseListed(_name, _address);
         } else {
@@ -213,24 +259,24 @@ contract CLETCORE is Ownable {
         }
     }
 
-    function releaseListed(string memory _name, address _address)
-        public
-        onlyOwner
-    {
+    function releaseListed(
+        string memory _name,
+        address _address
+    ) public onlyOwner {
         doReleaseListed(_name, _address);
     }
 
-    function validatorReleaseListed(string memory _name, address _address)
-        public
-        isValidator
-    {
+    function validatorReleaseListed(
+        string memory _name,
+        address _address
+    ) public isValidator {
         doReleaseListed(_name, _address);
     }
 
-    function doReleaseListed(string memory _name, address _address)
-        private
-        noNull(_name)
-    {
+    function doReleaseListed(
+        string memory _name,
+        address _address
+    ) private noNull(_name) {
         CletName storage cletname = s_CletNames[name_ToID[_name]];
         deleteMapped(_name, cletname.owner);
         cletname.owner = _address;
@@ -248,6 +294,7 @@ contract CLETCORE is Ownable {
                 delete address_OwnedNames[_owner][index];
             }
         }
+
         for (uint256 index = 0; index < s_Tickers.length; index++) {
             string memory name_ex = string.concat(
                 _name,
@@ -258,6 +305,13 @@ contract CLETCORE is Ownable {
             delete mapTo_name_ex[_address];
             delete name_ext_ToMap[name_ex];
         }
+
+        for (uint256 index = 0; index < address_Keys[_owner].length; index++) {
+            delete address_key_name[_owner][address_Keys[_owner][index]];
+            delete address_Keys[_owner][index];
+        }
+        delete address_Default[_owner];
+        delete name_Secret[_name];
     }
 
     function addTicker(
